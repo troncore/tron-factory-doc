3931b8661d2fb63bc50c9ea37255504cbf981271
康建城 <1063303665@qq.com>
2020-05-14 21:06:25 +0800

    chore jenkins build

--- publish.sh
+++ publish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install "8.10.1"
 nvm alias default "8.10.1"
 yarn global add vuepress
 yarn build
